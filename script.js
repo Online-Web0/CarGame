10af36039d71e11e90240d2cfb02cf8d6a3c6bc8
Victor Salazar <illegal.2.0.0.0.illegal@gmail.com>
2026-02-13 21:15:34 -0600

    Update script.js

--- script.js
+++ script.js
@@ -455,13 +455,16 @@ function computeSpawn() {
   }
 
   var start = cpSegs[0];
-  var forward = start.normal.clone();
 
-  if (cpSegs.length > 1) {
-    var chk = cpSegs[1];
-    var v = chk.mid.clone().sub(start.mid);
-    if (v.dot(forward) < 0) forward.multiplyScalar(-1);
-  }
+  // Use checkpoint segment direction instead of normal
+  var dirVec = start.b.clone().sub(start.a);
+  dirVec.normalize();
+
+  spawnX = start.mid.x;
+  spawnY = start.mid.y;
+
+  spawnDir = Math.atan2(dirVec.x, dirVec.y);
+}
 
   spawnX = start.mid.x + forward.x * 3;
   spawnY = start.mid.y + forward.y * 3;
